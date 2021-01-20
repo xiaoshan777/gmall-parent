d094ed5d9ab290ce6d23df950d931dc289b38896
高喜闪 <2629930428@qq.com>
2021-01-20 20:03:42 +0800

    1

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ProductApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoxishan
+ * @date 2021/1/20 18:09
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
